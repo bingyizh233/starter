777b5c492bfdff164bf18a57fde58dc5d386d18f
bingyizh233 <72293886+bingyizh233@users.noreply.github.com>
2024-09-17 22:41:30 -0700

    Update options.lua for clipboard

--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,9 @@
 require "nvchad.options"
 
 -- add yours here!
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
