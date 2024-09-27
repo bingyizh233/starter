cfe4e7299cfa6309c174ae96a7247229f719d49d
bingyi <bingyiz@nvidia.com>
2024-09-27 20:05:37 +0000

    add tablegen lsp server config

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -28,10 +28,22 @@ lspconfig.mlir_lsp_server.setup {
   on_attach = nvlsp.on_attach,
   capabilities = vim.lsp.protocol.make_client_capabilities(),
   cmd = {"/scratch/projects/triton/build/bin/triton-lsp"},
-  filetypes = {"mlir", "td"},
+  filetypes = {"mlir"},
   single_file_support = {true},
   root_dir = function(fname)
       return lspconfig.util.find_git_ancestor(fname) or vim.fn.getcwd()
   end,
 }
 
+
+
+lspconfig.tblgen_lsp_server.setup {
+  on_attach = nvlsp.on_attach,
+  capabilities = vim.lsp.protocol.make_client_capabilities(),
+  cmd = {"/scratch/triton-home/.triton/llvm/llvm-36adf8ec-ubuntu-x64/bin/tblgen-lsp-server", "--tablegen-compilation-database=/scratch/projects/triton/build/tablegen_compile_commands.yml"},
+  filetypes = {"tablegen"},
+  single_file_support = {true},
+  root_dir = function(fname)
+      return lspconfig.util.find_git_ancestor(fname) or vim.fn.getcwd()
+  end,
+}
